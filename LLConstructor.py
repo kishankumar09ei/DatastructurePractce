712d28ab0b3ee1857c80b95d9c02b11b832f1930
Kishan Kumar <kishankumar09ei@gmail.com>
2026-06-11 17:51:13 +0530

    initial commit

---
+++ LLConstructor.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+    
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+        return True
+
+
+my_ll=LinkedList(4)
+my_ll.append(6)
+my_ll.print_list()
+
+# print(my_ll.head.value)
+# print(my_ll.tail.value)
+
---
+++ rough.py
@@ -0,0 +1,2 @@
+https://medium.com/@chandu.bathula16/machine-learning-concept-9-71b6bd2ea77d#:~:text=%3D%3E%20Avg%20Word2Vec%20is%20an%20extension,representation%20for%20the%20entire%20text.
+
